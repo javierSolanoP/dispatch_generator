f2f39c81fb9955f17163268ef53b4dc4dfd0dd95
javierSolanoP <jmsolano24@misena.edu.co>
2021-12-01 15:17:39 -0500

    Se aniade formulario para aniadir usuarios

--- main_service/resources/views/components/AddUser.blade.php
+++ main_service/resources/views/components/AddUser.blade.php
@@ -2,7 +2,28 @@
     <div class="ilustration">
         <img src="../../images/form.svg" alt="Ilustración de formulario">
     </div>
-    <div class="container-input">
-        
+    <div class="container">
+        @csrf
+        <div class="container-input">
+            <input type="text" name="identification" placeholder=" identificacion...">
+            <input type="text" name="userName" placeholder=" nombre de usuario">
+        </div>
+        <div class="container-input">
+            <input type="text" name="name" placeholder=" nombres">
+            <input type="text" name="lastName" placeholder=" apellidos">
+        </div>
+        <div class="container-input">
+            <input type="password" name="password" placeholder=" contraseña">
+            <input type="password" name="confirmPassword" placeholder=" confirmar contraseña">
+        </div>
+        <div class="container-input">
+            <select name="role">
+                <option value="...">role...</option>
+            </select>
+            <select name="gender">
+                <option value="...">género...</option>
+            </select>
+        </div>
+        <input type="submit" name="send" value="Añadir">
     </div>
 </form>
